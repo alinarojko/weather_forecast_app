99e9ed6f3a916b8190ee6fe3386636f16cfa8b39
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-12 18:59:32 +0100

    Add chart for weather and images for Sky condition

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ import requests
 API_KEY = "7c490bd16f5c494d9c60e0826cd87935"
 
 # Define the function which will request from user 3 parameters
-def get_data(place, days=None, kind=None):
+def get_data(place, days):
 
     # Url is taken from the website like example
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
@@ -22,13 +22,7 @@ def get_data(place, days=None, kind=None):
     # So to see the value of the temperature for  user entered {days} , we filter it
 
     filtered_data = filtered_data[:8*days]
-
-    # Check for the type user selected "temperature or sky"
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__ == "__main__":
-    get_data(place="Tokio", days=3, kind="Temperature")
+    get_data(place="Tokio", days=3)
